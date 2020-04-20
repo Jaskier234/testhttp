499ba27fa03dc9b1543ff9cd140ec8e6718c87a0
artur234 <jaskier234@gmail.com>
2020-04-20 20:20:28 +0200

    Adding structure for storing http message and basic functions for constructing http message
    initialize_message()
    add_header()
    end_message()

--- http.c
+++ http.c
@@ -1,1 +1,58 @@
 #include "http.h"
+
+#define INITIAL_MESSAGE_SIZE 256 // TODO sprawdzić zalecenia standardu
+#define CRLF "\r\n"
+
+int initialize_http_message(http_message *message) {
+  message->message = malloc(sizeof(char) * INITIAL_MESSAGE_SIZE);
+  if (message->message == NULL) {
+    return -1;
+  } 
+  
+  message->length = 0;
+  message->capacity = INITIAL_MESSAGE_SIZE;
+
+  return 0;
+} 
+
+int add_header(http_message *message, char *header_name, char *value) {
+  int header_name_len = strlen(header_name);
+  int value_len = strlen(value);
+  // one char for ":", two for CRLF, one for NULL at the end and 4 more so that
+  // realloc in end_message is not needed.
+  int header_len = header_name_len + value_len + 8;
+
+  // realloc if capacity is too small
+  if (message->length + header_len > message->capacity) { 
+    int new_size = message->capacity * 2;
+    if (new_size < message->length + header_len) { // in case capacity*2 is to small
+      new_size = message->length + header_len;
+    }
+
+    message->message = realloc(message->message, new_size);
+    if (message->message == NULL) {
+      return -1;
+    }
+
+    message->capacity = new_size;
+  }
+
+  strcpy(message->message + message->length, header_name);
+  message->length + header_name_len;
+
+  strcpy(message->message + message->length, ":");
+  message->length + 1;
+  
+  strcpy(message->message + message->length, value);
+  message->length + value_len;
+
+  strcpy(message->message + message->length, CRLF);
+  message->length + 2;
+
+  return 0;
+}
+
+void end_message(http_message *message) {
+  strcpy(message->message + message->length, CRLF);
+  message->length + 2;
+}
--- http.h
+++ http.h
@@ -1,3 +1,20 @@
+/* Set of functions for constructing and parsing http messages */
 
-// TODO
+#include <stddef.h>
 
+// represents http message
+typedef struct {
+  char *message;
+  size_t length; // length of the message
+  size_t capacity; // size of allocated memory
+} http_message;
+
+// initializes http message
+int initialize_http_message(http_message*);
+
+// Adds header to http messsage
+// returns 0 on success or -1 if error occurs.
+int add_header(http_message* message, char* header, char *value);
+
+// Adds CRLF at the end of the message
+void end_message(http_message *message);
