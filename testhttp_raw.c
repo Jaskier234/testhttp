8b83b131c54e005a3d4d2ab0876bc3e03380ebd4
artur234 <jaskier234@gmail.com>
2020-04-23 23:03:06 +0200

    Basic reading response from server

--- testhttp_raw.c
+++ testhttp_raw.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "connect.h"
+#include "http.h"
 #include "err.h"
 
 int main(int argc, char *argv[]) {
 
-  if (argc < 3) {
-    fatal("Usage: %s host port message ...\n", argv[0]);
+  if (argc != 4) {
+    fatal("Usage: %s <address>:<port> <cookie file> <tested address> ...\n", argv[0]);
   }
+
+  http_message request;
+
+  initialize_http_message(&request);
+
+  if (-1 == generate_request(&request, argv[3], argv[2])) {
+    fatal("generate request failed");
+  }
+
+  int socket = make_connection(argv[1]);
+
+  write(socket, request.message, request.length);
+
+  char *buffer = malloc(sizeof(char) * 1000);
+
+  // TODO read in loop
+  read(socket, buffer, 1000);
+
+  printf("%s", buffer);
 
   return 0;
 }
